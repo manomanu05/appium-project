371f4b8b3eeee83db62f6f738e2cc8981155d3d6
manomanu05 <153796220+manomanu05@users.noreply.github.com>
2025-10-26 03:50:52 +0530

    Update test_sorting.py

--- testcases/test_sorting.py
+++ testcases/test_sorting.py
@@ -3,13 +3,13 @@ from PageObject.LoginPage import LoginPage
 from PageObject.SortingPage import SortingPage
 
 def test_login_and_apply_filter(driver):
-    # Step 1: Login
+    # Login
     login_page = LoginPage(driver)
     login_page.login("standard_user", "secret_sauce")
 
-    # Step 2: Open filter modal
+    #Open filter modal
     sorting_page = SortingPage(driver)
     sorting_page.open_filter_modal()
 
-    # Step 3: Select "Price (low to high)"
+    # Select "Price (low to high)"
     sorting_page.select_price_low_to_high()
